6ec6da06115a5f37ce0aa3273a4adeba5c29465c
Will Chen <will.chen@cloudkitchens.com>
2020-02-28 15:49:40 -0800

    Add setup.py.

---
+++ README.md
@@ -0,0 +1,11 @@
+# gack - Git Stack Manager
+
+gack helps a git user use git like Mercurial Queues: gack provide the ability to push/pop and create patches and automatically rebases them.
+
+Currently supported commands are:
+
+* init
+* deinit
+* push
+* pop
+* show
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="gack",
+    version="0.0.1",
+    author="Will Chen",
+    author_email="will@asianafrowill.com",
+    description="Gack - Git Stack manager",
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+    url="https://github.com/AsianAfroWill/gack",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'gitpython',
+    ],
+)
+
